f229ab92f09722a194d5a0fa5907c2dfefce3da2
gingeraffee <nicole.thornton330@gmail.com>
2026-03-03 09:56:27 -0600

    Update app.py

--- atp_streamlit/app.py
+++ atp_streamlit/app.py
@@ -13,7 +13,7 @@ import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="Attendance Point Tracker",
-    page_icon="📋",
+    page_icon="⏰",
     layout="wide",
     initial_sidebar_state="expanded",
 )
